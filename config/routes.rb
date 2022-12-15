1bb44ef42cb42256176692631865245630019ec6
Kevin Winarko <kevin.winarko@outlook.com>
2022-12-15 14:53:54 +0700

    setting routes user

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # api/v1/user
+
+  namespace :api do
+    namespace :v1 do
+      resources :user
+    end
+  end
 end
+ 
